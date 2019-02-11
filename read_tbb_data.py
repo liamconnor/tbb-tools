870ad8813225fe39c5329d4cef265144537e0142
Liam Connor <liam.dean.connor@gmail.com>
2019-02-11 11:23:52 +0100

    rewriting print summary data

--- read_tbb_data.py
+++ read_tbb_data.py
@@ -425,7 +425,8 @@ class TBBh5_Reader():
     return rcus, dipole_names
 
   def print_summary_data(self, fn):
-    pass
+    station_groups = self.get_stations_groups()
+    pritn(station_groups)
 
   def voltage_to_intensity(self, data_arr_volt):
     """ Assume last axis is time (Re, Im, Re, ...)
