4cda01fd157c7901cb139b0d7e1860bfd2a79018
Liam Connor <liam.dean.connor@gmail.com>
2019-02-27 13:32:49 +0100

    removed counter

--- read_tbb_data.py
+++ read_tbb_data.py
@@ -538,6 +538,7 @@ class TBBh5_Reader():
     for ii in range(len(dipole_sb_map)):
       sb = int(dipole_sb_map[ii][1][-3:])
       nsample_ii = len(data_rcu[ii])
+      print(rcu_set[rcu_set==dipole_sb_map[ii][0]])
       rcu_ind = np.where(dipole_sb_map[ii][0]==np.array(rcu_set))[0]
       print(ii, dipole_sb_map[ii][0], dipole_sb_map[ii][1], sb, rcu_ind)
       data_arr[:, sb, :nsample_ii] = data_rcu[ii]
