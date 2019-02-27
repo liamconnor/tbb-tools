bb14680f648424cf05cf9da1c2530813c4f59e3d
Liam Connor <liam.dean.connor@gmail.com>
2019-02-27 13:29:58 +0100

    removed counter

--- read_tbb_data.py
+++ read_tbb_data.py
@@ -532,13 +532,13 @@ class TBBh5_Reader():
   def construct_fullband_arr(self, data_rcu, dipole_sb_map, nsample, nrcu, rcu_set):
     data_arr = np.empty([nrcu, self.nsubband_full, 2*nsample])
 
-    np.sort(rcu_set)
+    rcu_set = np.sort(rcu_set)
     print(rcu_set)
 
     for ii in range(len(dipole_sb_map)):
       sb = int(dipole_sb_map[ii][1][-3:])
       nsample_ii = len(data_rcu[ii])
-      rcu_ind = np.where(dipole_sb_map[ii][0]==rcu_set)[0]
+      rcu_ind = np.where(dipole_sb_map[ii][0]==np.array(rcu_set)[0]
       print(ii, dipole_sb_map[ii][0], dipole_sb_map[ii][1], sb, rcu_ind)
       data_arr[:, sb, :nsample_ii] = data_rcu[ii]
 
