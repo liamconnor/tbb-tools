9f7d6db880df94b8fdf520854041b77d53753e11
Liam Connor <liam.dean.connor@gmail.com>
2019-10-24 15:59:33 +0200

    plotter reshape bug

--- plot_spectrum_h5.py
+++ plot_spectrum_h5.py
@@ -10,20 +10,21 @@ if __name__=='__main__':
 
     ax_dict = {0:'ndipole', 1:'subband', 2:'time'}
 
-    try:
-        axis = int(sys.argv[2])
-    except:
-        axis = 0
-
     T = read_tbb_data.TBBh5_Reader(fn)
     station_name = list(set(T.get_stations_groups()[1]))
-    data, mapping, tt = T.station_data(station_name[0], nsubband_tot=None)
+    data, mapping, tt = T.station_data(station_name[0], nsubband_tot=10)
     #data_I = T.voltage_to_intensity(data)
 
-    print("Data array shape is (ndipole, nsubband, nsample)=%s" % str((data.shape)))
-    print("Averaging over %s axis" % ax_dict[axis])
+    if axis is not 'all':
+        try:
+            axis = int(sys.argv[2])
+        except:
+            axis = 0
+        data = data.mean(axis)
 
-    data = data.mean(axis)
+        print("Data array shape is (ndipole, nsubband, nsample)=%s" % str((data.shape)))
+        print("Averaging over %s axis" % ax_dict[axis])
+
 
     if axis==0:
         ylab = 'SUBBAND'
@@ -35,12 +36,27 @@ if __name__=='__main__':
         ylab = 'DIPOLE INDEX'
         xlab = 'SUBBAND'     
 
-    fig2 = plt.figure()
-    plt.imshow(np.log10(data), aspect='auto', cmap='Greys')
-    plt.ylabel(ylab, fontsize=20)
-    plt.xlabel(xlab, fontsize=20)
-    plt.show()
-    
+    if axis is not 'all':
+        fig2 = plt.figure()
+        plt.imshow(np.log10(data), aspect='auto', cmap='Greys')
+        plt.ylabel(ylab, fontsize=20)
+        plt.xlabel(xlab, fontsize=20)
+        plt.show()
+    else:
+        fig2 = plt.figure()
+        plt.add_subplot(131)
+        plt.imshow(np.log10(data.mean(0)), aspect='auto', cmap='Greys')
+        plt.ylabel(ylab, fontsize=20)
+        plt.xlabel(xlab, fontsize=20)
+        plt.add_subplot(132)
+        plt.imshow(np.log10(data.mean(1)), aspect='auto', cmap='Greys')
+        plt.ylabel(ylab, fontsize=20)
+        plt.xlabel(xlab, fontsize=20)   
+        plt.add_subplot(133)
+        plt.imshow(np.log10(data.mean(2)), aspect='auto', cmap='Greys')
+        plt.ylabel(ylab, fontsize=20)
+        plt.xlabel(xlab, fontsize=20)
+        plt.show()
 #    times = T.get_time_arr(fn)
 #    plt.plot(t[-1]+4150*10*(freq**-2-freq[-1]**-2), linewidth=3.5, color='orange')
 #    plt.xlabel('Freq [MHz]', fontsize=15)
