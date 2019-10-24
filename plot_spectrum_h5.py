7d49a7c253e5915e830740a20f44a458b1ce8cef
Liam Connor <liam.dean.connor@gmail.com>
2019-10-24 14:24:01 +0200

    plotter reshape bug

--- plot_spectrum_h5.py
+++ plot_spectrum_h5.py
@@ -11,7 +11,6 @@ if __name__=='__main__':
     T = read_tbb_data.TBBh5_Reader(fn)
     station_name = list(set(T.get_stations_groups()[1]))
     data, mapping, tt = T.station_data(station_name[0])
-    np.save('data_here', data)
     data_I = T.voltage_to_intensity(data)
 
     data_tf = data_I.mean(0)
