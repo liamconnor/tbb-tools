699fc954cd3a1c09b82928088b2d1a70d0d238bd
Liam Connor <liam.dean.connor@gmail.com>
2020-04-12 15:12:48 +0200

    prints nicer

--- print_header.py
+++ print_header.py
@@ -4,5 +4,6 @@ import h5py
 
 fnh5 = sys.argv[1]
 
-f = h5py.File(fnh5)
-print(f["/SUB_ARRAY_POINTING_000/"]['BEAM_000'].attrs.items())
+f = h5py.File(fnh5,'r')
+for pair in f["/SUB_ARRAY_POINTING_000/"]['BEAM_000'].attrs.items():
+	print(pair)
